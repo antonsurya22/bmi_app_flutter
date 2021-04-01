22c1d22c9757dde8ffd712ea8081c7408f5e7556
I Gusti Nyoman Anton Surya Diputra <antonagung22@gmail.com>
2021-04-01 10:58:39 +0800

    first commmit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'Home.dart';
 
 void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
   theme: ThemeData.light(),
   home: InputBMI(),
 ));
